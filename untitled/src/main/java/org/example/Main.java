721debd58533a66d43cb4868eab07fe4f18ed798
GabrielC <Pukssito@gmail.com>
2022-09-29 12:15:25 +0200

    cambios de un try/catch

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -5,22 +5,23 @@ import java.util.concurrent.Semaphore;
 public class Main {
     public static void main(String[] args) {
 
-        Semaphore pingSemaphore = new Semaphore(1);
-        Semaphore pongSemaphore = new Semaphore(1);
-
-
-        Thread ping = new Thread(new PingTask(pingSemaphore, pongSemaphore));
-     Thread pong = new Thread(new PongTask(pingSemaphore, pongSemaphore));
-
         try {
+            Semaphore pingSemaphore = new Semaphore(1);
+            Semaphore pongSemaphore = new Semaphore(1);
+
+
+            Thread ping = new Thread(new PingTask(pingSemaphore, pongSemaphore));
+            Thread pong = new Thread(new PongTask(pingSemaphore, pongSemaphore));
+
+
             pongSemaphore.acquire();
+
+            ping.start();
+            pong.start();
+
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        ping.start();
-        pong.start();
-
-
 
     }
 
