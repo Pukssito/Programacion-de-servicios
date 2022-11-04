bb58a340424922f9249141b86f615739b90f8d37
GabrielC <96666808+Pukssito@users.noreply.github.com>
2022-11-04 09:50:17 +0100

    mikel ex

---
+++ ioc-tarea/src/main/java/components/MemberStorage.java
@@ -0,0 +1,29 @@
+package components;
+
+import entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberStorage {
+
+
+    private Map<String, Member> members = new HashMap<>();
+
+    public void addMember(Member member){
+
+        members.put(member.getNif(),member);
+    }
+    public void removeMember(String nif){
+        members.remove(nif);
+    }
+    public List<Member> listMembers(){
+        return members.values().stream().collect(Collectors.toList());
+    }
+}
